5d73071f8d7d21e9816bc4db5ca0bfea19f71b48
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-02-15 16:16:18 +0100

    af

--- MONGO/mongotesting.py
+++ MONGO/mongotesting.py
@@ -19,22 +19,30 @@ print(f"De naam van het eerste product is: {eerste_product['name']}, de prijs va
 #2a vraag 1
 print(f"Het eerste product is {eerste_product['name']} en kost: €{eerste_product['price']['selling_price']/100}")
 
+
 #2a vraag 2
+products.rewind()
 for product in products:
     if product['name'][0] == 'R':
         print(f"Het eerste product met de R is {product['name']}")
         break
+
+
 #2a vraag 3
+products.rewind()
 totaleprijs = 0
 productcount = 0
 for product in products:
     try:
         if product['price']['selling_price'] > 0:
+            if isinstance(product['price']['selling_price'], float): #if price is float, convert to int and count that
+                print('Inconsistente prijs gevonden, slaan we over')
+                continue
             totaleprijs += product['price']['selling_price']
             productcount += 1
     except KeyError:
-        print(f"Price niet aanwezig bij productid:{product['_id']}")
-print(f"De gemiddelde prijs van de producten is: €{round((totaleprijs/productcount))/100}. Er staan {productcount} producten met prijs in de database.")
+        pass
+print(f"De gemiddelde prijs van de producten is: €{round(totaleprijs/productcount)/100}. Er staan {productcount} producten met prijs in de database.")
 
 # zoeken met filter
 # products = mongodbDAO.getDocuments("products",{'category': 'Wonen & vrije tijd'})
